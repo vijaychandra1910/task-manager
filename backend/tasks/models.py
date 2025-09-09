c7c2787348fccdde30af3eadc5eda610d3ab2163
VIJAY KUMAR CHANDRA <vijaychandra356@gmail.com>
2025-09-09 14:38:49 +0530

    Initial commit - Fullstack Task Manager (Django + React)

---
+++ backend/tasks/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
